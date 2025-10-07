c75a524469f37cd68bc40820071aa53f9482cde0
Alexander Manriquez <Alexander.manriquez.s@gmail.com>
2025-10-07 11:41:29 -0300

    feat: set up Handlebars view engine and create initial views for contact management

--- app.js
+++ app.js
@@ -1,51 +1,27 @@
 const express = require('express');
-const { readTasks, addTask, updateTask, deleteTask } = require('./fileUtils');
+const path = require('path');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/tasks', (req, res) => {
-  const tasks = readTasks();
-  res.json(tasks);
-});
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
-app.post('/tasks', (req, res) => {
-  const { id, title, completed } = req.body;
+app.use(express.static(path.join(__dirname, 'public')));
 
-  if (!id || !title || completed === undefined) {
-    return res.status(400).json({ message: 'Faltan datos para poder enviar la tarea'});
-  }
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-  addTask({ id, title, completed });
-  res.status(201).json({ message: 'Tarea creada exitosamente' })
-});
-
-app.put('/tasks/:id', (req, res) => {
-  const taskId = req.params.id;
-  const updatedTask = req.body;
-
-  const result = updateTask(taskId, updatedTask);
-
-  if (result) {
-    res.json({ message: 'Tarea actualizada exitosamente', task: result });
-  } else {
-    res.status(404).json({ message: 'Tarea no encontrada' });
-  }
-});
-
-app.delete('/tasks/:id', (req, res) => {
-  const taskId = req.params.id;
-
-  const deleted = deleteTask(taskId);
-
-  if(deleted) {
-    res.json({ message: 'Tarea eliminada exitosamente' });
-  } else {
-    res.status(404).json({ message: 'Tarea no encontrada' });
-  }
-});
+app.get('/', (req, res) => {
+    res.render('home', { 
+      title: 'Tu Lista de contactos', 
+      description: 'Administra todos tus contactos en un solo lugar: agrega, edita y elimina contactos fácilmente.' 
+    });
+});    
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
